af4715948c91e9be1d3591b298652b87977717d2
PeachyVVolf <ahmadajmal9301@gmail.com>
2021-08-09 19:48:59 +0500

    Added MiniPalettes to Home Page Showing and Linking to all Palettes

---
+++ src/Palette.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import Navbar from './Navbar';
+import './css/Palette.css';
+
+class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { level: 500, format: "hex" }
+        this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat =  this.changeFormat.bind(this);
+    }
+
+    changeLevel(newLevel) {
+        this.setState({ level: newLevel })
+    }
+
+    changeFormat(val) {
+        this.setState({ format: val })
+    }
+
+    render() { 
+        const colorBoxes = this.props.palette.colors[this.state.level].map(color => (
+            <ColorBox background={color[this.state.format]} name={color.name} key={color.id}/>
+        ))
+        return (
+            <div className='Palette'>
+                <Navbar level={this.state.level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
+                <div className='Palette-colors'>
+                    {colorBoxes}
+                </div>
+                <footer className='Palette-footer'>
+                    {this.props.palette.paletteName}
+                    <span className='emoji'>{this.props.palette.emoji}</span>
+                </footer>
+            </div>
+        );
+    }
+}
+ 
+export default Palette;
